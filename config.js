76cd80db15c74a068ebc30746eb2037a11132e13
manickyoj <hotshot354@gmail.com>
2015-04-26 20:45:55 -0400

    Added HDMI to valid pin list. 49 pins confirmed available.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var GPIO = ['P8_7', 'P8_8', 'P8_9', 'P8_10', 'P8_11', 'P8_12', 'P8_13', 'P8_14',
             'P8_17', 'P8_18', 'P8_19', 'P8_26', 'P9_11', 'P9_12', 'P9_13', 'P9_14', 'P9_15', 'P9_16',
             'P9_21', 'P9_22', 'P9_23', 'P9_24', 'P9_26', 'P9_27', 'P9_30', 'P9_41', 'P9_42']
 
-// Only available if not connected to a monitor via HDMI
+// Only available if HDMI is disabled (all tested and operational)
 var HDMI = ['P8_27', 'P8_28', 'P8_29', 'P8_30', 'P8_31', 'P8_32', 'P8_33', 'P8_34', 'P8_35', 'P8_36',
             'P8_37', 'P8_38', 'P8_39', 'P8_40', 'P8_41', 'P8_42', 'P8_43', 'P8_44', 'P8_45', 'P8_46']
 
@@ -13,7 +13,7 @@ var eMMC = ['P8_3', 'P8_4', 'P8_5', 'P8_6', 'P8_20', 'P8_21', 'P8_22', 'P8_23', 
 // May or may not be available: need testing.
 var experimental = ['P9_33', 'P9_34', 'P9_35', 'P9_36', 'P9_37', 'P9_38', 'P9_39', 'P9_40']
 
-var PIN_LIST = GPIO;
+var PIN_LIST = GPIO.concat(HDMI);
 
 module.exports = {
     ROW_DURATION: 100,
